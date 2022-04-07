516dd1168de3707ca08ee759cdef849b3cd29c69
Sébastien Prunier <sebastien.prunier@gmail.com>
2022-04-07 15:03:50 +0200

    Ajout du swagger

--- demo-api/src/main/java/com/example/demo/controllers/DocumentationController.java
+++ demo-api/src/main/java/com/example/demo/controllers/DocumentationController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DocumentationController {
 
-
     @GetMapping("/doc")
     public String documentation() {
         return "documentation";
